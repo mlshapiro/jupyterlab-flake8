031bfd13021dc06645edc7161ceaccd8893f0df0
mls <mls@creare.com>
2018-04-11 14:52:46 -0400

    init plugin

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# jupyterlab-pylint
-pylint integration into jupyterlab
+# Jupyterlab-pylint
+
+Jupyterlab extension for integrating [pylint](https://pylint.readthedocs.io/en/latest/) 
+
+
+## Prerequisites
+
+- JupyterLab
+
+## Installation
+
+```bash
+jupyter labextension install jupyterlab-pylint
+```
+
+## Development
+
+For a development install (requires npm version 4 or later), do the following in the repository directory:
+
+```bash
+npm install
+npm run build
+jupyter labextension link .
+```
+
+To rebuild the package and the JupyterLab app:
+
+```bash
+npm run build
+jupyter lab build
+```
+
+To run jupyter lab in watch mode:
+
+```bash
+jupyter lab --watch
+```
+
+## Acknowledgment
+
+- Used https://github.com/ijmbarr/jupyterlab_spellchecker as a starting point
---
+++ src/index.ts
@@ -0,0 +1,213 @@
+import {
+  JupyterLab, JupyterLabPlugin
+} from '@jupyterlab/application';
+
+import {
+  INotebookTracker
+} from '@jupyterlab/notebook';
+
+import {
+  IMainMenu
+} from '@jupyterlab/mainmenu';
+
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
+  Terminal
+} from '@jupyterlab/terminal';
+
+import '../style/index.css';
+
+/**
+ * Linter
+ */
+class PyLinter {
+  app: JupyterLab;
+  tracker: INotebookTracker;
+  palette: ICommandPalette;
+  mainMenu: IMainMenu;
+  term: Terminal;
+
+  // Default Options
+  loaded: boolean = false;      // if pylint is available
+  enabled: boolean = true;    // turn on.off linter
+  pylintrc: string = '.pylintrc';    // name of pylintrc file
+
+  constructor(app: JupyterLab, tracker: INotebookTracker, palette: ICommandPalette, mainMenu: IMainMenu){
+   
+    this.app = app;
+    this.mainMenu = mainMenu;
+    this.tracker = tracker;
+    this.palette = palette;
+
+    // activate function when cell changes
+    this.tracker.activeCellChanged.connect(this.onActiveCellChanged, this);
+    
+    // add menu item
+    this.add_commands();
+
+    // if the linter is enabled, load it
+    if (this.enabled) {
+      this.load_linter();
+    }
+  }
+
+  /**
+   * Load pylint
+   */
+  async load_linter(){
+
+    // Bail if there are no terminals available.
+    if (!this.app.serviceManager.terminals.isAvailable()) {
+      console.log('Disabling pylinter plugin because it cant access terminal');
+      this.loaded = false;
+      this.enabled = false;
+      return;
+    }
+
+    this.term = new Terminal();
+    try {
+      this.term.session = await this.app.serviceManager.terminals.startNew();
+      console.log('loaded terminal session');
+      this.loaded = true;
+    }
+    catch(e) {
+      console.log('terminal failed');
+      this.term.dispose();
+      this.term = undefined;
+    }
+  }
+
+  /**
+   * Dispose of the terminal used to lint
+   */
+  dispose_linter() {
+    if (this.term) {
+      this.term.dispose();
+      console.log('disposed terminal');
+    } else {
+      console.log('no terminal to dispose');
+    }
+  }
+
+  /**
+   * Turn linting on/off
+   */
+  toggle_linter(){
+    this.enabled = !this.enabled;
+
+    if (this.enabled) {
+      this.load_linter();
+    } else {
+      this.dispose_linter();
+    }
+  }
+
+  /**
+   * Create palette menu button
+   */
+  add_commands(){
+    let category = 'Pylint';
+
+    // define all commands
+    let commands:any = {
+      'pylint:toggle': {
+        label: "Toggle pylint",
+        execute: () => {
+          this.toggle_linter();
+        } 
+      }
+    };
+
+    // add commands to menus and palette
+    for (let key in commands) {
+      this.app.commands.addCommand(key, commands[key]);
+      this.palette.addItem({command: key, category: category} );
+      this.mainMenu.viewMenu.addGroup([
+        { command: key }
+      ], 30);      
+    }
+  }
+
+  /**
+   * Run linter when active cell changes
+   */
+  onActiveCellChanged(): void {
+    
+    // only run when on a code cell
+    let active_cell = this.tracker.activeCell;
+    if ( (active_cell !== null) && (active_cell.model.type === 'code') ){
+      console.log('active cell changed and its code')
+      let editor: any = active_cell.editor;
+      let current_mode: string = editor.getOption('mode');
+      
+      if (this.enabled) {
+        editor.setOption("mode", this.define_mode(current_mode));
+      } else {
+
+        // not sure what this does yet
+        // let original_mode = (current_mode.match(/^spellcheck_/)) ? current_mode.substr(11) : current_mode
+        // editor.setOption("mode", original_mode);
+      }
+    }
+  }
+  //         'pylint@python',
+//         '--msg-template=\'{line}:{column}:{msg_id}: {msg} ({symbol})\'',
+//         '--module-rgx=.*',  # don't check the module name
+//         '--reports=n',      # remove tables
+// '--persistent=n', # don't save the old score (no sense for temp)
+// 
+
+  define_mode = (original_mode_spec: string) => {
+    console.log(original_mode_spec)
+      // if (original_mode_spec.indexOf("spellcheck_") == 0){
+      //     return original_mode_spec;
+      // }
+      // var me = this;
+      // var new_mode_spec = 'spellcheck_' + original_mode_spec;
+      // CodeMirror.defineMode(new_mode_spec, (config:any) => {
+      //     var spellchecker_overlay = {
+      //         name: new_mode_spec,
+      //         token: function (stream:any, state:any) {
+      //             if (stream.eatWhile(me.rx_word_char)){
+      //         var word = stream.current().replace(/(^')|('$)/g, '');
+      //         if (!word.match(/^\d+$/) && (me.dictionary !== undefined) && !me.dictionary.check(word)) {
+      //             return 'spell-error';
+      //                 }
+      //             }
+                  
+      //             stream.eatWhile(me.rx_non_word_char);
+      //             return null;
+      //         }
+      //     };
+      //     return CodeMirror.overlayMode(
+      //         CodeMirror.getMode(config, original_mode_spec), spellchecker_overlay, true);
+      // });
+      // return new_mode_spec;
+  }
+}
+
+
+/**
+ * Activate extension
+ */
+function activate(app: JupyterLab, tracker: INotebookTracker, palette: ICommandPalette, mainMenu: IMainMenu) {
+    console.log('jupyterlab-pylint activated');
+    const pl = new PyLinter(app, tracker, palette, mainMenu);
+    console.log('pylinter Loaded', pl);
+};
+
+
+/**
+ * Initialization data for the jupyterlab-pylint extension.
+ */
+const extension: JupyterLabPlugin<void> = {
+  id: 'jupyterlab-pylint',
+  autoStart: true,
+  activate: activate,
+  requires: [INotebookTracker, ICommandPalette, IMainMenu]
+};
+
+export default extension;
