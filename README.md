31ddedcee65a964faa8042dc4b66bf69ad2ae2d9
Marc Shapiro <marcus@tengr.net>
2018-04-11 22:41:18 -0400

    finish integrating the terminal - need to run linter on each change

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Jupyterlab-pylint
 
-Jupyterlab extension for integrating [pylint](https://pylint.readthedocs.io/en/latest/) 
-
+Jupyterlab extension for [Pylint](https://www.pylint.org/) 
 
 ## Prerequisites
 
@@ -15,26 +14,34 @@ jupyter labextension install jupyterlab-pylint
 
 ## Development
 
+It is advisable to use a seperate conda environment for development:
+
+```bash
+$ conda create -n jlpylint anaconda
+```
+
 For a development install (requires npm version 4 or later), do the following in the repository directory:
 
 ```bash
-npm install
-npm run build
-jupyter labextension link .
+$ npm install
+$ npm run build
+$ jupyter labextension install . --no-build  
 ```
 
 To rebuild the package and the JupyterLab app:
 
 ```bash
-npm run build
-jupyter lab build
+$ npm run build
+$ jupyter lab build
 ```
 
-To run jupyter lab in watch mode:
+To run jupyter lab and thet typescript in watch mode:
 
 ```bash
-jupyter lab --watch
+$ jupyter lab --watch       # in the first terminal window
+$ npm run watch             # in a new terminal window
 ```
+
 
 ## Acknowledgment
 
--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ class PyLinter {
 
   // Default Options
   loaded: boolean = false;      // if pylint is available
-  enabled: boolean = true;    // turn on.off linter
+  toggled: boolean = true;      // turn on.off linter
   pylintrc: string = '.pylintrc';    // name of pylintrc file
 
   constructor(app: JupyterLab, tracker: INotebookTracker, palette: ICommandPalette, mainMenu: IMainMenu){
@@ -44,18 +44,18 @@ class PyLinter {
 
     // activate function when cell changes
     this.tracker.activeCellChanged.connect(this.onActiveCellChanged, this);
-    
+
     // add menu item
     this.add_commands();
 
     // if the linter is enabled, load it
-    if (this.enabled) {
+    if (this.toggled) {
       this.load_linter();
     }
   }
 
   /**
-   * Load pylint
+   * Load terminal session and pylint
    */
   async load_linter(){
 
@@ -63,7 +63,7 @@ class PyLinter {
     if (!this.app.serviceManager.terminals.isAvailable()) {
       console.log('Disabling pylinter plugin because it cant access terminal');
       this.loaded = false;
-      this.enabled = false;
+      this.toggled = false;
       return;
     }
 
@@ -71,13 +71,70 @@ class PyLinter {
     try {
       this.term.session = await this.app.serviceManager.terminals.startNew();
       console.log('loaded terminal session');
-      this.loaded = true;
+
+      // wait 2 seconds for terminal to load and get initial commands out of its system
+      setTimeout(() => {
+        this.check_pylint();
+      }, 2000)
     }
     catch(e) {
-      console.log('terminal failed');
       this.term.dispose();
       this.term = undefined;
     }
+  }
+
+  /**
+   * Check to see if pylint is available on the machine
+   */
+  check_pylint() {
+    let self = this;
+
+    // need to figure out how to import ISession and IMessage
+    function onTerminalMessage(sender:any, msg:any): void {
+      let message:string = msg.content[0];
+      console.log(message);
+
+      // return if its just a message reflection
+      if (message.includes('which pylint')) {
+        return;
+      }
+
+      // if message includes pylint, then `which pylint` was successful and we can say the library is loaded
+      if (message.includes('pylint')) {
+        self.loaded = true;
+        self.activate_pylint();
+      } else {
+        alert('Pylint was not found on the machine. Install with `pip install pylint` or `conda install pylint`')
+        self.loaded = false;
+      }
+
+      // remove this listener from the terminal session
+      self.term.session.messageReceived.disconnect(onTerminalMessage)
+    }
+
+    // listen for stdout in onTerminalMessage and ask `which pylint`
+    this.term.session.messageReceived.connect(onTerminalMessage);
+    this.term.session.send({type: 'stdin', content: ['which pylint\r']})
+  }
+
+  /**
+   * Activate pylint terminal reader
+   */
+  activate_pylint() {
+    // listen for stdout in onLintMessage
+    this.term.session.messageReceived.connect(this.onLintMessage);
+  }
+
+  
+  /**
+   * Handle terminal message during linting
+   * // need to figure out how to import ISession and IMessage
+   * @param {any} sender [description]
+   * @param {any} msg    [description]
+   */
+  onLintMessage(sender:any, msg:any): void {
+    let message:string = msg.content[0];
+    console.log(message);
   }
 
   /**
@@ -85,6 +142,7 @@ class PyLinter {
    */
   dispose_linter() {
     if (this.term) {
+      this.term.session.messageReceived.disconnect(this.onLintMessage); 
       this.term.dispose();
       console.log('disposed terminal');
     } else {
@@ -96,9 +154,9 @@ class PyLinter {
    * Turn linting on/off
    */
   toggle_linter(){
-    this.enabled = !this.enabled;
+    this.toggled = !this.toggled;
 
-    if (this.enabled) {
+    if (this.toggled) {
       this.load_linter();
     } else {
       this.dispose_linter();
@@ -106,7 +164,7 @@ class PyLinter {
   }
 
   /**
-   * Create palette menu button
+   * Create menu / command items
    */
   add_commands(){
     let category = 'Pylint';
@@ -115,6 +173,8 @@ class PyLinter {
     let commands:any = {
       'pylint:toggle': {
         label: "Toggle pylint",
+        isEnabled: () => { return this.loaded},
+        isToggled: () => { return this.toggled},
         execute: () => {
           this.toggle_linter();
         } 
@@ -139,14 +199,16 @@ class PyLinter {
     // only run when on a code cell
     let active_cell = this.tracker.activeCell;
     if ( (active_cell !== null) && (active_cell.model.type === 'code') ){
-      console.log('active cell changed and its code')
+
+      console.log('active code cell changed')
       let editor: any = active_cell.editor;
       let current_mode: string = editor.getOption('mode');
       
-      if (this.enabled) {
+      if (this.toggled) {
+        // this.term.session.send({type: 'stdin', content: ['which pylint\r']})
+
         editor.setOption("mode", this.define_mode(current_mode));
       } else {
-
         // not sure what this does yet
         // let original_mode = (current_mode.match(/^spellcheck_/)) ? current_mode.substr(11) : current_mode
         // editor.setOption("mode", original_mode);
@@ -194,9 +256,9 @@ class PyLinter {
  * Activate extension
  */
 function activate(app: JupyterLab, tracker: INotebookTracker, palette: ICommandPalette, mainMenu: IMainMenu) {
-    console.log('jupyterlab-pylint activated');
-    const pl = new PyLinter(app, tracker, palette, mainMenu);
-    console.log('pylinter Loaded', pl);
+  console.log('jupyterlab-pylint activated');
+  const pl = new PyLinter(app, tracker, palette, mainMenu);
+  console.log('pylinter Loaded', pl);
 };
 
 
